c16f139ffd70ff429b03c4d00d789d84d49c0399
AngelicChicken <yuvelimcheris2003@gmail.com>
2024-06-28 01:27:15 +0700

    make 16x16 grid using js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello")
+const container = document.querySelector("#container");
+
+let amountOfGrid = 0;
+while(amountOfGrid < 256){
+    const grid = document.createElement("div");
+    grid.setAttribute("id", "grid")
+
+    container.appendChild(grid);
+
+    amountOfGrid++
+}
