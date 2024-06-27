902e45e3e962609871bb491c22ac0bd7f7753ee7
AngelicChicken <yuvelimcheris2003@gmail.com>
2024-06-28 04:25:51 +0700

    modify code so row and column always have the right dimension

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 const container = document.querySelector("#container");
 
-let amountOfGrid = 0;
-while(amountOfGrid < 256){
-    const grid = document.createElement("div");
-    grid.setAttribute("id", "grid")
+let gridRowAmount = 0;
+let gridColumnAmount = 0;
 
-    container.appendChild(grid);
+while(gridColumnAmount < 16){
+    //create place to store the grid
+    const gridColumn = document.createElement("div");
+    const gridRow = document.createElement("div");
 
-    amountOfGrid++
+    //if there's less than 16 grid in a row, create more grid
+    while(gridRowAmount < 16){
+        const grid = document.createElement("div");
+        grid.setAttribute("id", "grid");
+        
+        //add made grid to row
+        gridRow.appendChild(grid);
+    
+        gridRowAmount++
+    }
+
+    //reset amount of grid in a row so next row can be generated
+    gridRowAmount = 0;
+
+    //add row to the column
+    gridColumn.appendChild(gridRow);
+    //add column to overall grid container
+    container.appendChild(gridColumn);
+    gridColumnAmount++
 }
